41379ad1bd4004906b30a9c62de199ffdd2b4983
Davi <daviavr@gmail.com>
2024-05-28 16:14:14 -0300

    edicao da geracao de populacoes iniciais

--- common/individual_generators.py
+++ common/individual_generators.py
@@ -1,14 +1,17 @@
-from common.individual import Individual
-from scipy.stats import qmc,binom
-from math import cos
+from scipy.stats import qmc
 import random
 import numpy as np
 
-def convert_01_to_range(floor:int, ceiling:int, random_number:int):
-    return round(((ceiling - floor) * random_number) + floor)
+def quantization(floor:int, ceiling:int, random_number:int):
+    discretizations = (ceiling - floor) + 1 
+    for discrete_number in range(floor, discretizations):
+        numerator = discrete_number + 1 if floor == 0 else discrete_number  
+        if random_number <= numerator/discretizations:
+            break
+    return discrete_number
 
 class ChaosMaps:
-    salt = random.random() * (10 ** -10)
+    salt = random.random()
     current_value = None
     def __init__(self,variables_range,num_of_variables):
         self.floor = min(variables_range)
@@ -17,43 +20,28 @@ class ChaosMaps:
 
     
     def generic_logistic_map(self):
-        r = 3.999999301 + ChaosMaps.salt
+        r = 3.999999301
         
-        if ChaosMaps.current_value == None:
-            features = [0.254561]
-            for n in range(8):
-                features.append(r * features[n] * (1-features[n]))
-            features = [features[-1]]
-        else:
-            features = [ChaosMaps.current_value] 
+        features = [ChaosMaps.salt if ChaosMaps.current_value == None else ChaosMaps.current_value] 
         
         for n in range(self.num_of_variables-1):
             features.append( r*features[n]*(1-features[n]) )
 
         ChaosMaps.current_value = features[-1]
-        features = [convert_01_to_range(self.floor,self.ceiling,i) for i in features]
+        features = [quantization(self.floor,self.ceiling,i) for i in features]
         return features
    
     def cosin_map_generation(self):
-        r = 6 + ChaosMaps.salt
+        r = 6
         
-        if ChaosMaps.current_value == None:
-            features = [0.1]
-            for n in range(8):
-                features.append( cos(r*features[n]) )
-            features = [features[-1]]
-        else:
-            features = [ChaosMaps.current_value] 
-        
+        features = [ChaosMaps.salt if ChaosMaps.current_value == None else ChaosMaps.current_value] 
+
         for n in range(self.num_of_variables-1):
-            features.append( cos(r*features[n]) )
+            features.append( np.cos(r*features[n]) )
         
         ChaosMaps.current_value = features[-1]
         
-        if self.floor == 0 and self.ceiling == 1:
-            features = [0 if i <= 0 else 1 for i in features]
-        else:
-            features = [convert_01_to_range(self.floor,self.ceiling,abs(i)) for i in features]
+        features = [quantization(self.floor,self.ceiling,abs(i)) for i in features]
         return features
 
 class BetaFunctionVariations:
@@ -63,11 +51,6 @@ class BetaFunctionVariations:
         self.num_of_variables = num_of_variables
 
     def latin_hypercube(self):
-        features_01 = qmc.LatinHypercube(d=1).random(self.num_of_variables).flatten()
-        features = [convert_01_to_range(floor=self.floor,ceiling=self.ceiling,random_number=n) for n in features_01]
-        return features
-
-    def binomial(self):
-        binomial_generation = binom((self.ceiling - self.floor), 0.4, loc=self.floor)
-        features = binomial_generation.rvs(size=self.num_of_variables)
-        return features
+        features = qmc.LatinHypercube(d=1).random(n=self.num_of_variables).flatten()
+        features_discretized = [quantization(self.floor,self.ceiling,num) for num in features]
+        return features_discretized
