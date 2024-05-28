89b2d4b13ee5c54411b80866fa775f3f9f1f7b5a
Davi <daviavr@gmail.com>
2024-05-28 16:44:01 -0300

    script

---
+++ run-tcc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#transform the list of files into the array format
+
+allBenchs=("KNAPSACK" "U-KNAPSACK" "MINMAX")
+numFuncs=4
+
+tmux new-session -d  -c "$HOME/Documentos/programming/python/idlhc-code" -s ${allBenchs[$1]}
+for ((i=0; i < $numFuncs; i++)); do
+	tmux new-window -t ${allBenchs[$1]} "$HOME/.conda/envs/idlhc_env/bin/python main_tcc.py 30 $i ${allBenchs[$1]} 0 1"
+	echo ""
+done
+
